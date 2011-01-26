342d660ae129b42f4ad9e4239316d78a9312ef16
Antoine Pultier <antoine@blogjaune.fr>
2011-01-26 12:57:01 +0100

    okaux

--- src/polytech/stock/SQL/GestionSQL.java
+++ src/polytech/stock/SQL/GestionSQL.java
@@ -2,7 +2,102 @@ package polytech.stock.SQL;
 
 import polytech.stock.GestionnaireDeStock;
 
+import java.util.List;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
 public abstract class GestionSQL implements GestionnaireDeStock
 {
 
+	protected static Connection connexion = null;
+	protected Statement declaration = null;
+	protected PreparedStatement declarationPreparee;
+
+	protected static void seConnecterSiNecessaire()
+	{
+		
+		// Chargement du driver JDBC bourrin (traduction de sqlite en java,
+		// et bibliothèques natives pour les architectures répandues)
+		try
+		{
+			// Si il y a déjà une connexion, inutile d'aller plus loin
+			if (connexion != null && !connexion.isClosed())
+			{
+				return;
+			}
+
+			Class.forName("org.sqlite.JDBC");
+		
+			// Connexion
+			connexion = DriverManager.getConnection("jdbc:sqlite:canard.db");
+
+		} catch (Exception e)
+		{
+			System.out.println("Problème d'ouverture de la base de données : "+e.getMessage());
+		}
+	}
+
+	protected static void seDeconnecter()
+	{
+		try{
+			if (connexion != null && !connexion.isClosed())
+			{
+				connexion.close();
+			}
+		} catch (Exception e)
+		{
+			System.out.println("Problème de fermeture de la base de données : "+e.getMessage());
+		}
+	}
+
+	// Informations sur la table
+	protected abstract String structureTable();
+	protected abstract String nomTable();
+	protected abstract int nbInfosTable();
+
+	protected <CLASS_TYPE> void sauvegarderDansBase(List<CLASS_TYPE> liste)
+	{
+		try{
+			String nom = nomTable();
+			String structure = structureTable();
+
+			declaration = connexion.createStatement();
+			declaration.executeUpdate("DROP TABLE IF EXISTS "+nom+";");
+			declaration.executeUpdate("CREATE TABLE "+nom+" ("+structure+");");
+
+			StringBuilder sb = new StringBuilder("INSERT INTO ");
+			sb.append(nom);
+			sb.append(" (");
+			sb.append(structure);
+			sb.append(") VALUES (");
+
+			int nbInfos = nbInfosTable();
+			for (int i = 0; i < nbInfos; ++i)
+			{
+				sb.append("?,");
+			}
+
+			sb.deleteCharAt(sb.length());
+
+			sb.append(");");
+
+			declarationPreparee = connexion.prepareStatement(sb.toString());
+
+			for (CLASS_TYPE e : liste)
+			{
+				if (construirePourStock(e) != null)
+				{
+					declarationPreparee.executeUpdate();
+				}
+			}
+
+		} catch (SQLException e)
+		{
+			System.out.println("Impossible de sauvegarder : "+e.getMessage());
+		}
+	}
 }
--- src/polytech/stock/SQL/OrganisateurSQL.java
+++ src/polytech/stock/SQL/OrganisateurSQL.java
@@ -1,20 +1,43 @@
 package polytech.stock.SQL;
 
+import java.util.List;
+
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+import polytech.personnes.Organisateur;
+
 public class OrganisateurSQL extends GestionSQL
 {
+
+	protected List<Organisateur> organisateurs;
+
+	protected String structureTable()
+	{
+		return "NOM TEXT, PRENOM TEXT, MOTDEPASSE TEXT";
+	}
+
+	protected String nomTable()
+	{
+		return "organisateurs";
+	}
+
+	protected int nbInfosTable()
+	{
+		return 3;
+	}
 
 	@Override
 	public void chargerStock()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void sauvegarderStock()
 	{
-		// TODO Auto-generated method stub
-
+		GestionSQL.seConnecterSiNecessaire();
+		sauvegarderDansBase(organisateurs);
 	}
 
 	@Override
@@ -41,8 +64,20 @@ public class OrganisateurSQL extends GestionSQL
 	@Override
 	public Object construirePourStock(Object element)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Organisateur a = (Organisateur) element;
+
+		try
+		{
+			declarationPreparee.setString(1, a.getNom()); 
+			declarationPreparee.setString(2, a.getPrenom()); 
+			declarationPreparee.setString(3, a.getPassword()); 
+		} catch (SQLException e)
+		{
+			System.out.println("Impossible de créer un élement du stock : "+e.getMessage());
+			return null;
+		}
+
+		return declarationPreparee;
 	}
 
 }
